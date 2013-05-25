1ac9f319a325fab8c8b184da3835e7101d07ce4c
Vagner Leitte <vagner.leite.silva@gmail.com>
2013-05-25 10:59:53 -0300

    Whathever

--- TCC/src/util/SendEmail.java
+++ TCC/src/util/SendEmail.java
@@ -1,46 +1,37 @@
 package util;
 
-import java.util.Properties;  
-import javax.mail.Message;  
-import javax.mail.Session;  
-import javax.mail.Transport;  
-import javax.mail.internet.InternetAddress;  
-import javax.mail.internet.MimeMessage;   
-import javax.mail.Authenticator;  
-import javax.mail.PasswordAuthentication;   
-
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
-import org.apache.commons.mail.Email;
 
-  
-public class SendEmail {  
-      
-    
+public class SendEmail {
+
 	SimpleEmail email = new SimpleEmail();
+
 	// Utilize o hostname do seu provedor de email
 
-	public SendEmail(String smtpAddress, int Port){
+	public SendEmail(String smtpAddress, int Port) {
 
 		email.setHostName(smtpAddress);
 		// Quando a porta utilizada não é a padrão (gmail = 465)
 		email.setSmtpPort(Port);
-		
+
 	}
-	
+
 	// Adicione os destinatários
-	public boolean GetAddress(String Email, String Nome){
-	
-			try {
-				email.addTo(Email, Nome);
-			} catch (EmailException e) {
-				e.printStackTrace();
-			}
-		return false;	
+	public boolean setAddress(String Email, String Nome) {
+
+		try {
+			email.addTo(Email, Nome);
+			
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
-	
-	public void Sendmail(String From, String Subject, String Msg, String Authentication, boolean SSL, String Pass){
-		
+
+	public void Sendmail(String From, String Subject, String Msg,
+			String Authentication, boolean SSL, String Pass) {
+
 		try {
 			email.setFrom(From, "JTicketMaster");
 		} catch (EmailException e) {
@@ -56,16 +47,14 @@ public class SendEmail {
 		}
 		email.setSSL(SSL);
 		email.setAuthentication(Authentication, Pass);
-		
+
 		try {
 			email.send();
 		} catch (EmailException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	} 
-	
-	
-	
+
+	}
+
 }
